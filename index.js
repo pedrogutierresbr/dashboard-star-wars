ab3cb23b2221a6429aa780b60b195586499d22c7
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-10 15:08:47 -0300

    adicionado grafico de pizza

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ const navesContador = document.getElementById('naves');
 preencherContadores();
 preencherTabela();
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(desenharGrafico);
+
+async function desenharGrafico() {
+    const response = await swapiGet('vehicles/')
+    const vehiclesArray = response.data.results;
+    console.log(vehiclesArray);
+
+    const dataArray = [];
+    dataArray.push(['Veículos', 'Passageiros']);
+    vehiclesArray.forEach(vehicle => {
+        dataArray.push([vehicle.name, Number(vehicle.passengers)]);
+    })
+
+    var data = google.visualization.arrayToDataTable(dataArray);
+
+    var options = {
+        title: 'Maiores veículos',
+        legend: 'none'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
+
 function preencherContadores(){
     Promise.all([swapiGet('people/'), 
                 swapiGet('species/'),
@@ -22,7 +48,6 @@ function preencherContadores(){
 async function preencherTabela(){
     const response  = await swapiGet('films/');
     const tableData = response.data.results;
-    console.log(tableData);
 
     tableData.forEach(film => {
         $("#filmsTable").append(`<tr>
